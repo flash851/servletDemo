48db12e9f6547ae2f4608e659216729fb9e93e6d
Anuj Yadav <anujyadav851@gmail.com>
2021-05-15 01:27:16 +0530

    Reading HTML form data from Servlet

---
+++ src/com/flash/servletDemo/StudentServlet.java
@@ -0,0 +1,55 @@
+package com.flash.servletDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		//Step 1: set the content type
+		response.setContentType("text/html");
+		
+		//Step 2. get the printwriter
+		PrintWriter out=response.getWriter();
+		
+		//Step 3. generate html Content
+		out.println("<html><body>");
+		out.println("The Student confirmed is: "+request.getParameter("firstName")+" "+request.getParameter("lastName"));
+		out.println("<hr>");
+		out.println("The time on server is: "+new java.util.Date());
+		out.println("</html></body>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
